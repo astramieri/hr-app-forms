da86c486813d825ae5d520a081fef65307f86bb0
Angelo Stramieri <angelo.stramieri@gmail.com>
2024-12-05 10:38:38 +0100

    Update app

--- bin/fsal.bat
+++ bin/fsal.bat
@@ -1,2 +1,6 @@
+REM Add the following parameters to application configuration section
+REM baseSAAfile=webutilsaa.txt
+REM fsalcheck=true
+
 REM Forms Application Deployment Services (FADS)
 java -jar ..\jar\frmsal.jar -url "http://localhost:9001/forms/frmservlet?config=hr-app"
