c2adf3e27702a15ace50a06cfbdcc7a0cbd3135b
Angelo Stramieri <angelo.stramieri@gmail.com>
2024-12-05 10:27:18 +0100

    Update app

--- bin/fsal.bat
+++ bin/fsal.bat
@@ -1,2 +1,2 @@
 REM Forms Application Deployment Services (FADS)
-java -jar ..\jar\frmsal.jar -url "http://localhost:9001/forms/frmservlet?config=webstart_fsal"
+java -jar ..\jar\frmsal.jar -url "http://localhost:9001/forms/frmservlet?config=hr-app"
