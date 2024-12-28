963fd370109c4ae2261ee82005c79e11667e4c50
Angelo Stramieri <angelo.stramieri@gmail.com>
2024-12-28 11:13:01 +0100

    Update app

--- usr/compile.sh
+++ usr/compile.sh
@@ -12,5 +12,7 @@ export BIN_DIR=/home/oracle/hr-app-forms/bin
 export DB_STRING=hr/oracle@freepdb1
 
 $FORMS_INSTANCE/bin/frmcmp.sh module_type=form module=$SRC_DIR/general.fmb output_file=$BIN_DIR/general.fmx userid=$DB_STRING batch=yes compile_all=yes 
+$FORMS_INSTANCE/bin/frmcmp.sh module_type=form module=$SRC_DIR/employees.fmb output_file=$BIN_DIR/employees.fmx userid=$DB_STRING batch=yes compile_all=yes 
+$FORMS_INSTANCE/bin/frmcmp.sh module_type=form module=$SRC_DIR/departments.fmb output_file=$BIN_DIR/departments.fmx userid=$DB_STRING batch=yes compile_all=yes 
 
-git restore $SRC_DIR/general.fmb
+git restore $SRC_DIR/*.fmb
