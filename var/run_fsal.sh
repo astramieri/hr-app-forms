084df840e668035815426d6fd548df2463641993
Angelo Stramieri <angelo.stramieri@gmail.com>
2024-12-28 12:11:26 +0100

    Update app

--- var/run_fsal.sh
+++ var/run_fsal.sh
@@ -1,6 +1,7 @@
-REM Add the following parameters to application configuration section
-REM baseSAAfile=webutilsaa.txt
-REM fsalcheck=true
+#!/bin/bash
 
-REM Oracle Forms Stand Alone Launcher (FSAL)
+# Add the following parameters to application configuration section
+# baseSAAfile=webutilsaa.txt
+# fsalcheck=true
+
 java -jar "../lib/frmsal.jar" -url "http://localhost:9001/forms/frmservlet?config=hr-app"
