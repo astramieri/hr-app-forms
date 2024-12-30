b962f6de9b497b23a7a9dd37848784f3469813fa
Angelo Stramieri <angelo.stramieri@gmail.com>
2024-12-30 17:48:01 +0100

    Update app

--- usr/cmp.sh
+++ usr/cmp.sh
@@ -11,6 +11,7 @@ mkdir -p $BIN_DIR
 
 $FORMS_INSTANCE/bin/frmcmp.sh module_type=form module=$SRC_DIR/general.fmb output_file=$BIN_DIR/general.fmx userid=$DB_CONNECT_STRING batch=no compile_all=yes 
 $FORMS_INSTANCE/bin/frmcmp.sh module_type=form module=$SRC_DIR/employees.fmb output_file=$BIN_DIR/employees.fmx userid=$DB_CONNECT_STRING batch=no compile_all=yes 
+$FORMS_INSTANCE/bin/frmcmp.sh module_type=form module=$SRC_DIR/employee_edit.fmb output_file=$BIN_DIR/employee_edit.fmx userid=$DB_CONNECT_STRING batch=no compile_all=yes 
 $FORMS_INSTANCE/bin/frmcmp.sh module_type=form module=$SRC_DIR/departments.fmb output_file=$BIN_DIR/departments.fmx userid=$DB_CONNECT_STRING batch=no compile_all=yes 
 
 git restore $SRC_DIR/*.fmb
