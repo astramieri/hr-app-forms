23167941d9727d65b0a00f44d67356a2a9e788db
Angelo Stramieri <angelo.stramieri@gmail.com>
2024-12-31 16:11:46 +0100

    Update app

--- usr/cmp.sh
+++ usr/cmp.sh
@@ -10,9 +10,8 @@ BIN_DIR=$BASE_DIR/../bin
 mkdir -p $BIN_DIR
 
 $FORMS_INSTANCE/bin/frmcmp.sh module_type=form module=$SRC_DIR/login.fmb output_file=$BIN_DIR/login.fmx userid=$DB_CONNECT_STRING batch=no compile_all=yes 
-$FORMS_INSTANCE/bin/frmcmp.sh module_type=form module=$SRC_DIR/general.fmb output_file=$BIN_DIR/general.fmx userid=$DB_CONNECT_STRING batch=no compile_all=yes 
-$FORMS_INSTANCE/bin/frmcmp.sh module_type=form module=$SRC_DIR/employees.fmb output_file=$BIN_DIR/employees.fmx userid=$DB_CONNECT_STRING batch=no compile_all=yes 
+$FORMS_INSTANCE/bin/frmcmp.sh module_type=form module=$SRC_DIR/console.fmb output_file=$BIN_DIR/console.fmx userid=$DB_CONNECT_STRING batch=no compile_all=yes 
+$FORMS_INSTANCE/bin/frmcmp.sh module_type=form module=$SRC_DIR/employee_list.fmb output_file=$BIN_DIR/employee_list.fmx userid=$DB_CONNECT_STRING batch=no compile_all=yes 
 $FORMS_INSTANCE/bin/frmcmp.sh module_type=form module=$SRC_DIR/employee_edit.fmb output_file=$BIN_DIR/employee_edit.fmx userid=$DB_CONNECT_STRING batch=no compile_all=yes 
-$FORMS_INSTANCE/bin/frmcmp.sh module_type=form module=$SRC_DIR/departments.fmb output_file=$BIN_DIR/departments.fmx userid=$DB_CONNECT_STRING batch=no compile_all=yes 
 
 git restore $SRC_DIR/*.fmb
